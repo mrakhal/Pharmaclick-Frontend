facf77d0e16625526c93aa308b2b7c8389c8924f
allysarh <allysa.rahagustiani@gmail.com>
2021-07-19 15:22:58 +0700

    FE-ALLYSA-01: Configuration

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class LandingPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <div>
+                <h1>Test Landing Page</h1>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
