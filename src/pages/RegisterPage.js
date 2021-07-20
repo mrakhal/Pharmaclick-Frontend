ab8af593533072c76f827e7ed4072f18495bd406
allysarh <allysa.rahagustiani@gmail.com>
2021-07-20 14:39:31 +0700

    FE-ALLYSA-04: PHR-12 Menambahkan proteksi pada registrasi

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -22,7 +22,7 @@ class RegisterPage extends React.Component {
             email: '',
             nama: '',
             loading: false,
-            invalid: ''
+            invalid: []
         }
     }
 
@@ -33,21 +33,31 @@ class RegisterPage extends React.Component {
         try {
             let { nama, username, email, password } = this.state
             if (nama == '' || username == '' || email == '' || password == '') {
-                this.setState({ invalid: 'p-invalid' })
-                this.msgs2.show({ severity: 'error', summary: 'Error: .', detail: `Fill all the form!`, sticky: true })
+                this.toast2.show({ severity: 'error', summary: 'Error: .', detail: `Fill all the form!`, sticky: true });
                 return null
             } else {
-                if ((email.includes(".com") || email.includes(".co.id")) && email.includes("@")) {
-                    if (password.match(/a-z0-9/ig) && password.length > 6) {
-                        let register = await axios.post(URL_API + '/user/register', { nama, username, email, password })
-                        this.setState({ loading: true })
-                        this.setState({ loading: false })
-                        this.showSticky(register.data.messages, 'success')
+                if (username.length >= 6) {
+                    if (email.match(/(\.com|\.co|\.id)/ig) && email.includes('@')) {
+                        if (password.match(/^(?=.*[a-z].*)(?=.*[0-9].*)[a-z0-9]{6,}$/ig)) {
+                            this.setState({ loading: true })
+                            // cek apakah email sudah terdaftar
+                            let getEmail = await axios.get(URL_API + `/user/get?email=${email}`)
+                            if(getEmail.data.length == 0){
+                                let register = await axios.post(URL_API + '/user/register', { nama, username, email, password })
+                                this.showSticky(register.data.messages, 'success')
+                                this.toast2.show({ severity: 'success', summary: 'Register ', detail: ' Register success', sticky: true })
+                            } else {
+                                this.toast2.show({ severity: 'warn', summary: 'Warning ', detail: ' Email already registerd!', sticky: true })
+                            }
+                            this.setState({ loading: false })
+                        } else {
+                            this.toast2.show({ severity: 'warn', summary: 'Invalid: ', detail: ' Invalid format password!', sticky: true })
+                        }
                     } else {
-                        this.msgs2.show({ severity: 'warn', summary: 'Invalid: ', detail: ' Invalid format password!', sticky: true })
+                        this.toast2.show({ severity: 'warn', summary: 'Invalid: ', detail: ' Invalid format email!', sticky: true })
                     }
                 } else {
-                    this.msgs2.show({ severity: 'warn', summary: 'Invalid: ', detail: ' Invalid format email!', sticky: true })
+                    this.toast2.show({ severity: 'warn', summary: 'Invalid: ', detail: ' Username minimum 6 chars!', sticky: true })
                 }
             }
 
@@ -66,10 +76,12 @@ class RegisterPage extends React.Component {
                 </ul>
             </React.Fragment>
         );
+        const regex = new RegExp(/^(?=.*[a-z].*)(?=.*[0-9].*)[a-z0-9]{6,}$/, 'i', 'g')
         let { nama, username, email, password, invalid } = this.state
         return (
-            <div className="container-fluid" style={{ height: '95vh', width: '100%', backgroundImage: 'linear-gradient(to bottom, white , #f0edfe)', overflow: 'hidden' }}>
+            <div className="container-fluid" style={{ height: '100vh', width: '100%', backgroundImage: 'linear-gradient(to bottom, white , #f0edfe)', overflow: 'hidden' }}>
                 <Toast ref={(el) => this.toast = el} />
+                <Toast ref={(el) => this.toast2 = el} />
                 <div className="row " style={{ width: '92%', height: '75vh', borderRadius: '30px', backgroundColor: 'white', boxShadow: '5px 5px 7px grey', margin: 'auto', marginTop: '50px' }}>
                     <div className="col-6 d-none d-md-flex justify-content-center align-items-center" style={{ padding: '0 60px', height: '100%', borderRadius: '30px', backgroundColor: '#3264d6' }}>
                         <img src={doctorWoman} style={{ height: '80%' }} />
@@ -87,27 +99,32 @@ class RegisterPage extends React.Component {
                                 <Messages ref={(el) => this.msgs2 = el} />
                                 <div>
                                     <label className="p-d-block">Name</label>
-                                    <InputText value={nama} onChange={(e) => this.setState({ nama: e.target.value })} />
-                                    <small id="username1-help" className="p-d-block" style={{ fontSize: '10px' }}>Enter your full name </small>
+                                    <InputText value={nama} onChange={(e) => this.setState({ nama: e.target.value })} className="p-inputtext-sm" />
+                                    <small id="username1-help" className={this.state.invalid[0]} style={{ fontSize: '10px' }}>Enter your full name </small>
                                 </div>
                             </div>
                             <div className="p-fluid row">
                                 <div className="p-field d-flex flex-column col-6 col-md-6">
-                                    <label htmlFor="username1" className="p-d-block" style={{ fontSize: '20px' }}>Username</label>
-                                    <InputText aria-describedby="username1-help" className="p-d-block" value={username} onChange={(e) => this.setState({ username: e.target.value })} />
-                                    <small id="username1-help" className="p-d-block" style={{ fontSize: '10px' }}>Enter your username. </small>
+                                    <label className="p-d-block">Username</label>
+                                    <InputText className="p-d-block  p-inputtext-sm" value={username} onChange={(e) => this.setState({ username: e.target.value })} style={{border: username ? (username.length >= 6 ? '1px solid black' : '1px solid red') : '1px solid black'}}/>
+                                    {username ? (username.length >= 6 ? <small style={{ fontSize: '10px', color: 'green' }}>Username valid!</small> : <small style={{ color: 'red', fontSize: '10px' }}>Invalid username. Minumum 6 chars</small>) : <small style={{ fontSize: '10px' }}>Enter your username.</small>}
+
                                 </div>
                                 <div className="p-field d-flex flex-column col-6 col-md-6">
-                                    <label htmlFor="username1" className="p-d-block">Email</label>
-                                    <InputText id="username1" aria-describedby="username1-help" className="p-d-block" value={this.email} onChange={(e) => this.setState({ email: e.target.value })} />
-                                    <small id="username1-help" className="p-d-block" style={{ fontSize: '10px' }}>Enter your email. </small>
+                                    <label className="p-d-block">Email</label>
+                                    <InputText className="p-d-block  p-inputtext-sm" value={this.email} onChange={(e) => this.setState({ email: e.target.value })} style={{ border: email ? (email.match(/(\.com|\.co|\.id)/ig) && email.includes('@')) ? '1px solid black' : '1px solid red' : '1px solid black' }} />
+                                    {
+                                        email ? (email.match(/(\.com|\.co|\.id)/ig) && email.includes('@')) ? <small style={{ fontSize: '10px', color: 'green' }}>Email valid!</small> : <small style={{ fontSize: '10px', color: 'red' }}>Email invalid</small> : <small style={{ fontSize: '10px' }}>Enter your email.</small>
+                                    }
                                 </div>
                             </div>
                             <div className="p-fluid row">
                                 <div>
-                                    <label htmlFor="username1" className="p-d-block">Password</label>
-                                    <Password value={this.state.password} onChange={(e) => this.setState({ password: e.target.value })} footer={footer} toggleMask />
-                                    <small id="username1-help" className="p-d-block" style={{ fontSize: '10px' }}>Enter your password. </small>
+                                    <label className="p-d-block">Password</label>
+                                    <Password value={password} onChange={(e) => this.setState({ password: e.target.value })} feedback={false} toggleMask className="p-password-meter-info-sm" inputStyle={{ border: password ? password.match(regex) ? '1px solid black' : '1px solid red' : '1px solid black' }} />
+                                    {
+                                        password ? password.match(regex) ? <small style={{ fontSize: '10px', color: 'green' }}>Password valid!</small> : <small style={{ fontSize: '10px', color: 'red' }}>Password invalid. At least 6 characters contatin 1 alphabet and 1 number</small> : <small style={{ fontSize: '10px' }}>Enter your password.</small>
+                                    }
                                 </div>
                             </div>
                             <Button label="Sign Up" className="p-button-warning my-2" onClick={this.onBtnSignup} loading={this.state.loading} />
